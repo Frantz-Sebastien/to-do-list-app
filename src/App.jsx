c3edcd339b44e3b4547e3231716e7fb01cfb730d
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2025-02-02 00:52:13 -0500

    understanding tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './components/Todo'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <h1 className='bg-gray-600 text-white'>To Do List</h1>
-    </>
+    <div className='bg-stone-400 grid py-4 min-h-screen'>
+      <Todo />
+    </div>
   )
 }
 
---
+++ src/components/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-x1'>
+        <h1>Todo</h1>
+    </div>
+  )
+}
+
+export default Todo
