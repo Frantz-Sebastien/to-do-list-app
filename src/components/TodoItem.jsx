338ab309f5d8021bb0de7c9b2eb9e11ca91863ec
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2025-02-06 22:01:50 -0500

    fixed typo

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,11 +3,11 @@ import empty_square from '../assets/empty-square.png'
 import check_mark from '../assets/check-mark.png'
 import trash from '../assets/trash.png'
 
-const TodoItem = ({text, id, completedTask, deleteTask}) => {
+const TodoItem = ({text, id, completedTask, deleteTask, toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
         <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
-            <img src={empty_square} alt="image of a small empty square" className='w-7 h-7' />
+            <img src={completedTask ? check_mark : empty_square} alt="image of a small empty square" className='w-7 h-7' />
             <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
         </div>
 
