17ffe0264275b9179ace9a5d266868beef7cc221
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2025-02-06 22:12:12 -0500

    adding strikethrough

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -8,7 +8,7 @@ const TodoItem = ({text, id, completedTask, deleteTask, toggle}) => {
     <div className='flex items-center my-3 gap-2'>
         <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
             <img src={completedTask ? check_mark : empty_square} alt="image of a small empty square" className='w-7 h-7' />
-            <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
+            <p className={`text-slate-700 ml-4 text-[17px] ${completedTask ? "line-through": ""}`}>{text}</p>
         </div>
 
         <img src={trash} alt="image of a small trash can" className='w-4' onClick={()=>{deleteTask(id)}} />
